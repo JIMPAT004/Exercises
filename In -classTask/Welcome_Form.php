1596c42bdd61a686b44cbd1049a043903ed3b656
Jimil Patel <jimilpatel11@gmail.com>
2022-06-01 13:35:54 -0400

    Second commit

---
+++ In -classTask/Welcome_Form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome <?php echo $_POST['name']; ?></title>
+</head>
+<body>
+
+<p>Hello <?php echo $_POST['name'] ?>,</p>
+<p><?php echo $_POST['email'] ?> is your email.</p>
+
+</body>
+</html>
